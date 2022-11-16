6ba661f6d3d8865f49a29656bec346a914ec1023
Angelo31294 <luongoangelo1@gmail.com>
2022-11-16 15:38:48 +0100

    Add Api Boolean

--- js/script.js
+++ js/script.js
@@ -5,7 +5,19 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        email: ''
+        emails: []
       }
+    },
+    methods: {
+
+    },
+    created() {
+        for(let i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response);
+                this.emails.push(response.data.response);
+            });
+        }
     }
   }).mount('#app')
