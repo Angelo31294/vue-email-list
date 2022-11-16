4f236ce83fe8f9ac20be075dd1a2e9372e1fb358
Angelo31294 <luongoangelo1@gmail.com>
2022-11-16 15:46:28 +0100

    Add MILESTONE 1 - Fine

--- js/script.js
+++ js/script.js
@@ -15,8 +15,9 @@ const { createApp } = Vue
         for(let i = 1; i <= 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                console.log(response.data.response);
-                this.emails.push(response.data.response);
+                // console.log(response.data.response);
+                const email = response.data.response;
+                this.emails.push(email);
             });
         }
     }
