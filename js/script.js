4fd94243cc66761eef6d0a8fa484ce1419109000
Angelo31294 <luongoangelo1@gmail.com>
2022-11-17 10:35:26 +0100

    Correzione

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@
 
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        emails: []
-      }
-    },
-    methods: {
-
-    },
-    created() {
-        for(let i = 1; i <= 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                // console.log(response.data.response);
-                const email = response.data.response;
-                this.emails.push(email);
-            });
-        }
+createApp({
+  data() {
+    return {
+      emails: []
     }
-  }).mount('#app')
+  },
+  methods: {
+
+  },
+  created() {
+      for(let i = 1; i <= 10; i++) {
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((response) => {
+              // console.log(response.data.response);
+              const email = response.data.response;
+              this.emails.push(email);
+          });
+      }
+  }
+}).mount('#app')
